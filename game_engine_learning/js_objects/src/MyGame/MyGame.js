66d980ae748e1cfbe802527656e6e093f5f9548f
bdon-htb <brandonphillips2001@gmail.com>
2021-02-13 16:12:39 -0500

    Forgot to push these awhile ago

---
+++ game_engine_learning/js_objects/src/MyGame/MyGame.js
@@ -0,0 +1,18 @@
+function MyGame(htmlCanvasID) {
+  // The shader for drawing
+  this.mShader = null;
+
+  gEngine.Core.initializeWebGL(htmlCanvasID);
+
+  this.mShader = new SimpleShader("VertexShader", "FragmentShader");
+
+  // Step C1: Clear the canvas
+  gEngine.Core.clearCanvas([0, 0.8, 0, 1]);
+
+  // Step C2: Activate the proper shader
+  this.mShader.activateShader();
+
+  //Step C3: Draw the currently activated geometry and the activated shader.
+  var gl = gEngine.Core.getGL();
+  gl.drawArrays(gl.TRAINGLE_STRIP, 0, 4);
+}
